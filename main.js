c0e07de2455a1a3b3901e99eccee0aebd9f5089e
wuhx <benjiaminwu@outlook.com>
2020-05-12 21:04:33 +0800

    支持touch监听touch方法，否则mouse方法

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ function autoSetCanvasSize() {
 
   //监听页面尺寸变更
   window.onresize = () => {
-      setCanvasFullScreen()
-    }
-    //让canvas全屏
+    setCanvasFullScreen()
+  }
+  //让canvas全屏
   function setCanvasFullScreen() {
     var {
       clientWidth, clientHeight
@@ -28,8 +28,53 @@ function autoSetCanvasSize() {
 }
 
 function listenToMouse() {
-  // 点击鼠标
-  canvas.onmousedown = e => {
+  // 支持touch监听touch方法，否则mouse方法
+  if (document.body.ontouchstart !== undefined) {
+    console.log('触摸事件')
+    // 触屏开始摸
+    canvas.ontouchstart = function (e) {
+      brushEnabled = true
+      let {
+        clientX, clientY
+      } = e.touches[0]
+      if (eraserEnabled) {
+        ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
+      } else {
+        lastPoint = {
+          x: clientX,
+          y: clientY
+        }
+        drawCircle(clientX, clientY, 2)
+      }
+    }
+    // 触屏边摸边动
+    canvas.ontouchmove = function (e) {
+      if (!brushEnabled) {
+        return
+      }
+      let {
+        clientX, clientY
+      } = e.touches[0]
+      if (eraserEnabled) {
+        ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
+      } else {
+        let newPoint = {
+          x: clientX,
+          y: clientY
+        }
+        drawCircle(clientX, clientY, 2)
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+    // 触屏摸完了
+    canvas.ontouchend = function (e) {
+      brushEnabled = false
+    }
+  } else {
+    console.log('鼠标事件')
+    // 点击鼠标
+    canvas.onmousedown = e => {
       brushEnabled = true
       let {
         clientX, clientY
@@ -45,32 +90,34 @@ function listenToMouse() {
       }
     }
     // 移动鼠标
-  canvas.onmousemove = e => {
-    if (!brushEnabled) {
-      return
-    }
-    let {
-      clientX, clientY
-    } = e
-    if (eraserEnabled) {
-      ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
-    } else {
-      let newPoint = {
-        x: clientX,
-        y: clientY
+    canvas.onmousemove = e => {
+      if (!brushEnabled) {
+        return
       }
-      drawCircle(clientX, clientY, 2)
-      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-      lastPoint = newPoint
+      let {
+        clientX, clientY
+      } = e
+      if (eraserEnabled) {
+        ctx.clearRect(clientX - 5, clientY - 5, 10, 10)
+      } else {
+        let newPoint = {
+          x: clientX,
+          y: clientY
+        }
+        drawCircle(clientX, clientY, 2)
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+
     }
 
+    // 松开鼠标 
+    canvas.onmouseup = e => {
+      brushEnabled = false
+    }
   }
 
-  // 松开鼠标 
-  canvas.onmouseup = e => {
-    brushEnabled = false
 
-  }
 }
 
 
